b25f518d5eb7a371ea39fd2f31f0c38462aeed8a
Dayanna <dlvaron77@ucatolica.edu.co>
2026-05-31 03:11:56 -0500

    [ADD]Archivo con la conexión a las bases de datos

---
+++ db.py
@@ -0,0 +1,25 @@
+import os
+from contextlib import asynccontextmanager
+from dotenv import load_dotenv
+from sqlmodel import Session, create_engine, SQLModel
+from fastapi import FastAPI, Depends
+from typing import Annotated
+
+load_dotenv()
+
+DATABASE_URL = os.getenv("DATABASE_URL")
+engine = create_engine(DATABASE_URL)
+
+
+@asynccontextmanager
+async def create_all_tables(app: FastAPI):
+    SQLModel.metadata.create_all(engine)
+    yield
+
+
+def get_session() -> Session:
+    with Session(engine) as session:
+        yield session
+
+
+SessionDep = Annotated[Session, Depends(get_session)]
