7a373e4097302051c1932ed58742f23e68af1afe
Dayanna <dlvaron77@ucatolica.edu.co>
2026-06-01 19:30:03 -0500

    [UPLOAD]corrección de conexión a base de datos con verificación de conexión

--- db.py
+++ db.py
@@ -8,7 +8,7 @@ from typing import Annotated
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, pool_pre_ping=True)
 
 
 @asynccontextmanager
